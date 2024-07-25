bf55823cc22c370b47e170dede68166697e76987
Saurabh Rawat <yores.2002@gmail.com>
2024-07-25 20:02:01 +0530

    multihead functionality

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ class InputEmbeddings(nn.Module):
 
     def __init__(self,d_model:int,vocab_size:int):
         # d_model is the dimension of the model
-        super().__init__
+        super().__init__()
         self.d_model=d_model
         self.vocab_size=vocab_size
         self.embedding=nn.Embedding(vocab_size,d_model)
@@ -19,7 +19,7 @@ class InputEmbeddings(nn.Module):
 class PositionalEncoding(nn.Module):
 
     def __init__(self,d_model:int,seq_len:int,dropout:float)->None:
-        super().__init__
+        super().__init__()
         self.d_model=d_model
         self.seq_len=seq_len
         self.dropout=nn.Dropout(dropout)
@@ -61,7 +61,7 @@ class PositionalEncoding(nn.Module):
 class LayerNormalization(nn.Module):
 
     def __init__(self,eps:float=10**-6 ) -> None:
-        super().__init__
+        super().__init__()
         self.eps=eps
         # nn.Parameter makes the parameter learnable
         self.alpha=nn.Parameter(torch.ones(1)) #multiplied
@@ -77,7 +77,7 @@ class LayerNormalization(nn.Module):
 class FeedForwardBlock(nn.Module):
 
     def __init__(self, d_model:int,d_ff:int,dropout:float) -> None:
-        super().__init__
+        super().__init__()
         self.linear_1=nn.Linear(d_model,d_ff) #W1 and B1
         self.dropout=nn.Dropout(dropout)
         self.linear_2=nn.Linear(d_ff,d_model) #W2 and B2
@@ -89,4 +89,59 @@ class FeedForwardBlock(nn.Module):
 
                              
 
-    def
+class MutiHeadAttentionBloack(nn.Module):
+
+    def __init__(self, d_model:int,h:int,dropout:float) -> None:
+        super().__init__()
+        self.d_model=d_model
+        self.h=h
+        assert d_model%h == 0, "d_model is not divisible by h"
+        self.dk=d_model//h
+        self.w_q=nn.Linear(d_model,d_model)
+        self.w_k=nn.Linear(d_model,d_model)
+        self.w_v=nn.Linear(d_model,d_model)
+
+        self.w_o=nn.Linear(d_model,d_model)
+        self.dropout=nn.Dropout(dropout)
+    @staticmethod
+    # what is a static method-mtlb ki hum iss function ko kahi se bhi call krskte h without creating a instance
+    # we can just say MultiHeadAttentionBlock.attention()
+    def attention(Qp,Vp,Kp,mask,dropout:nn.Dropout):
+        dk=Qp.shape[-1]
+        attention_scores=(Qp @ Kp.transpose(-2,-1))/math.sqrt(dk)
+        if mask is not None:
+            attention_scores.masked_fill_(mask==0,-1e9)
+        # Yaha masking lagare taki jab softmax lge isme toh woh un values ko nullify krde
+
+        attention_scores=attention_scores.softmax(dim=-1) #(Batch,h,seq_len,seqlen)
+
+        if dropout is not None:
+            attention_scores=dropout(attention_scores)
+        return (attention_scores @ Vp),attention_scores
+
+
+
+    def forward(self,q,k,v,mask):
+        Qp=self.w_q(q) # (batch,seqlen,d_model)-->(batch,seqlen,d_model)
+        Kp=self.w_k(k) # (batch,seqlen,d_model)-->(batch,seqlen,d_model)
+        Vp=self.w_v(v) # (batch,seqlen,d_model)-->(batch,seqlen,d_model)
+        # Yaha prr humne multiply krra h
+
+        # (batch,seqlen,d_model)-->(batch,seqlen,h,dk)-->(batch,h,seqlen,dk)
+        # Yaha prr humne alag alag usme divide krdiya h
+        Qp=Qp.view(Qp.shape[0],Qp.shape[1],self.h,self.dk).transpose(1,2)
+        Vp=Vp.view(Vp.shape[0],Vp.shape[1],self.h,self.dk).transpose(1,2)
+        Kp=Kp.view(Kp.shape[0],Kp.shape[1],self.h,self.dk).transpose(1,2)
+
+        x,self.attention_scores=MutiHeadAttentionBloack.attention(Qp,Kp,Vp,mask,self.dropout)
+        # (batch,h,seq_len,dk )-->(batch,seq_len,h,dk)-->(batch,seqlen,dmodel)
+        x=x.transpose(1,2).contiguous().view(x.shape[0],-1,self.h*self.dk)
+
+
+        # (batch,seqlen,dmodel)-->(batch,seqlen,dmodel)
+        return self.w_o(x)
+    
+
+
+
+
