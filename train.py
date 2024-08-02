03e33d6ce08ad8217a39f1ddcf56a209d0a2b371
Saurabh Rawat <yores.2002@gmail.com>
2024-08-02 12:42:07 +0530

    config file created

--- train.py
+++ train.py
@@ -1,6 +1,9 @@
 import torch
 import torch.nn as nn
 
+
+from model import build_transformer
+from dataset import BilingualDataset,causal_mask
 from datasets import load_dataset
 from tokenizers import Tokenizer
 from tokenizers.models import WordLevel
@@ -44,4 +47,24 @@ def get_ds(config):
     val_ds_size=len(ds_raw)-train_ds_size
 
     train_ds_raw,val_ds_raw=random_split(ds_raw,[train_ds_size,val_ds_size])
+
+    train_ds=BilingualDataset(train_ds_raw,tokenizer_src,tokenizer_tgt,config['lang_src'],config['lang_tgt'],config['seq_len'])
+    val_ds=BilingualDataset(val_ds_raw,tokenizer_src,tokenizer_tgt,config['lang_src'],config['lang_tgt'],config['seq_len'])
     
+    # calculating the max seq len
+    max_len_src=0
+    max_len_tgt=0
+
+    for item in ds_raw:
+        src_ids=tokenizer_src.encode(item['translation'][config['lang_src']]).ids
+        tgt_ids=tokenizer_src.encode(item['translation'][config['lang_tgt']]).ids
+        max_len_src=max(max_len_src,src_ids)
+        max_len_tgt=max(max_len_tgt,tgt_ids)
+    print(f'Max length of source length: {max_len_src}')
+    print(f'Max len of target sentence: {max_len_tgt}')
+
+
+def get_model(config,vocab_src_len,vocab_tgt_len):
+    model=build_transformer(vocab_src_len,vocab_tgt_len,config['seq_len'],config['seq_len'],config['d_model'])
+    return model
+# if model is too big reduce head or number of layers
